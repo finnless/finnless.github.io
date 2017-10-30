4b6d5d5ec7e357cb2abccc519075424d7f1708ba
finnless <n@newroads.org>
2017-10-30 09:48:34 -0700

    Fixes and stuff

--- ticktacktoescript.js
+++ ticktacktoescript.js
@@ -2,11 +2,28 @@ var char = 'X';
 
 //THis function handles the onclick event
 function turn(location) {
+
+  //switch between X and O
+  if (char == 'X') {
+    char = 'O';
+  }
+  else{
+    char = 'X';
+  }
+
   document.getElementById(location).innerHTML = char;
 
 }
 
 function clear() {
-  document.document.getElementsByClassName(box).innerHTML = "";
+  document.getElementById('r1c1').innerHTML = "";
+  document.getElementById('r1c2').innerHTML = "";
+  document.getElementById('r1c3').innerHTML = "";
+  document.getElementById('r2c1').innerHTML = "";
+  document.getElementById('r2c2').innerHTML = "";
+  document.getElementById('r2c3').innerHTML = "";
+  document.getElementById('r3c1').innerHTML = "";
+  document.getElementById('r3c2').innerHTML = "";
+  document.getElementById('r3c3').innerHTML = "";
 
 }
