fbde8909202115b3714db083a817b0ea0978247d
finnless <n@newroads.org>
2017-10-30 09:33:31 -0700

    Tic Tac Toe
    Update Priv Policy
    Added tic tac toe page and resources.

---
+++ ticktacktoescript.js
@@ -0,0 +1,12 @@
+var char = 'X';
+
+//THis function handles the onclick event
+function turn(location) {
+  document.getElementById(location).innerHTML = char;
+
+}
+
+function clear() {
+  document.document.getElementsByClassName(box).innerHTML = "";
+
+}
