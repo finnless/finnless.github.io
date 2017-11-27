871bfd506a40077150786dcd72ebab7db7b7a3d6
finnless <n@newroads.org>
2017-11-27 09:49:56 -0800

    Initial Clickable
    First commit for Clickable project

---
+++ clickable.js
@@ -0,0 +1,25 @@
+//draws random circle in clickPanel
+function randomCircle(){
+
+    //randomly get the top positions
+
+    //randomly get left positions
+
+    //update position of cirlce
+
+    document.getElementById("circle").style.top = //random;
+
+}
+
+
+//handles onclick event
+//when a random circle is clicked in clickPanel, it disappears
+//then generates a new randomCircle
+function clickCircle(){
+
+    //circle is clicked, hide circle
+    document.getElementById("circle").style.visibility = "hidden";
+
+    //generate random circle
+
+}
