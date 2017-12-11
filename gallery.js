394ca188fb9cdc0820396f0202c5e8da614ab621
finnless <n@newroads.org>
2017-12-11 09:25:36 -0800

    Working Gallery
    Finished Gallery functioning

--- gallery.js
+++ gallery.js
@@ -1,21 +1,35 @@
-var list = ["https://i.imgur.com/M7JqWp1.jpg", "https://i.redd.it/zbpszcphju101.jpg", "https://i.redd.it/0yq1u44vcw101.jpg", "https://i.imgur.com/7adqsLr.jpg", "https://i.redd.it/bqjvrehhmkhz.jpg"]
+var list = ["https://i.imgur.com/M7JqWp1.jpg",
+"https://i.imgur.com/7adqsLr.jpg",
+"https://i.redd.it/bqjvrehhmkhz.jpg",
+"https://images.pexels.com/photos/296282/pexels-photo-296282.jpeg?h=350&auto=compress&cs=tinysrgb",
+"https://scontent-lax3-1.cdninstagram.com/t51.2885-15/e35/25011185_1992221364381747_834165382838222848_n.jpg",
+"https://scontent-lax3-1.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/24332040_391326191295875_6718900675830349824_n.jpg", "https://media.giphy.com/media/SxXKRsI6QG3nO/giphy.gif"]
 var Pic = 0;
-
+var length = list.length;
 
 function load(){
 
-  document.getElementById("canvas").src=list[Pic];
+  document.getElementById("canvas").src = list[Pic];
 
 }
 
 
 
 function goLeft(){
-Pic = Pic + 1;
-load()
+  if (Pic>0){
+    Pic = Pic - 1;
+  } else {
+    Pic = length;
+  }
+  load();
 }
 
 function goRight(){
-Pic = Pic + 1;
-load()
+  if (Pic<length){
+    Pic = Pic + 1;
+    load()
+  } else {
+    Pic = 0;
+  }
+  load();
 }
